1a115587159b99b069fede5ae18d7cfed2cc6d86
Diego De Sogos <diegodesogos@gmail.com>
2014-03-20 16:50:02 -0300

    Fixes to data model, replaced ManyToMany relationship for a ListField containing the indexes to the cards (as ManyToMany would require a JOIN operation which is forfeited in NoSQL database like AppEngine Datastore
    Added minimal test case that validate game data model construction.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from djangotoolbox.fields import ListField
 
 class GameState():
     """game states"""
@@ -28,19 +30,37 @@ class Card(models.Model):
 
 class PlayerGameState(models.Model):
     game = models.ForeignKey('Game', related_name='playergamestates')
+    order = models.IntegerField(default=0)
     player = models.ForeignKey(Player)
     player_state_id = models.CharField(max_length=400)
     points = models.IntegerField(default=0)
-    cards = models.ManyToManyField(Card, related_name='playerstatecards+')
+    cards = ListField(models.ForeignKey(Card))
     def __unicode__(self):
         return 'PlayerGameState: player ' + self.player.name + ' Points: ' +self.points.str() + ' Cards: ' + self.cards.str()
  
 class Game(models.Model):
     board_id = models.CharField(max_length=400)
-    creation_date = models.DateTimeField('date created')
+    creation_date = models.DateTimeField(default=datetime.now, blank=True)
     current_state = models.CharField(max_length=40,
                                       choices=GameState.CURRENT_GAME_STATE_CHOICES,
                                       default=GameState.WAITING_PLAYING_CARD_AND_CONCEPT)
+    def current_storyteller_playergamestate(self):
+        '@rtype PlayerGameState'
+        rounds = self.rounds.order_by('order')
+        if not rounds:
+            #get first PlayerGameState from player game states
+            return self.playergamestates.order_by('order')[0]
+        else:
+            #get the storyteller from the last round played
+            last_round = rounds.reverse()[0]
+            last_round_player = last_round.storyteller_player
+            return self.playergamestates.get(player=last_round_player)
+    def next_storyteller_playergamestate(self):
+        '@rtype PlayerGameState'
+        current_storyteller_order = self.current_storyteller_playergamestate().order
+        next_storyteller_order = (current_storyteller_order + 1) % self.playergamestates.count
+        return self.playergamestates.get(order=next_storyteller_order)
+            
     def __unicode__(self):
         return 'Game: unique id ' + self.board_id + \
                 ' Game state: ' + self.current_state
@@ -50,14 +70,18 @@ class PlayerPlay(models.Model):
     owner_player = models.ForeignKey(Player, related_name='plays')
     selected_card = models.ForeignKey(Card, related_name='+')
     unchosen_cards = models.ManyToManyField(Card, related_name='unchosen_cards+') 
-    voted_by_players = models.ManyToManyField(Player, related_name='vote_by_players+')   
+    voted_by_players = models.ManyToManyField(Player, related_name='vote_by_players+')
+    storyteller = models.BooleanField(default=False)   
     def __unicode__(self):
         return 'PlayerPlay: owner ' + self.owner_player.name + \
+                ' Storyteller: ' + self.storyteller.str() + \
                 ' Selected card ' + self.selected_card.url + \
                 ' Unchosen cards: ' + self.unchosen_cards.str()
     
 class GameRound(models.Model):
     game = models.ForeignKey(Game, related_name='rounds')
+    order = models.IntegerField(default=0)
+    opened = models.BooleanField(default=True)   
     storyteller_player = models.ForeignKey(Player, related_name='+')
     storyteller_selected_card = models.ForeignKey(Card, related_name='+')
     storyteller_sentence_for_selected_card = models.CharField(max_length=1000)
--- core/tests.py
+++ core/tests.py
@@ -6,11 +6,43 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from core.models import Game, Card, Player, PlayerGameState
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GameTest(TestCase):
+    def setUp(self):
+        '@type game: Game'
+        game = Game.objects.create(board_id='board 1')
+        for idx in range(0, 20):
+            Card.objects.create(url='url' +`idx`, description='card '+`idx`)
+        for idx in range(0, 3):
+            a_player= Player.objects.create(name='player' +`idx`)
+            player_order = idx
+            '@type a_playerstate: PlayerGameState'
+            a_playerstate = game.playergamestates.create(order=player_order,player=a_player, player_state_id='playerstate_player_url'+`idx`)
+            for cardidx in range(idx, idx+5):
+                card_id = idx*5 + cardidx
+                card = Card.objects.get(url='url'+`card_id`)
+                print card.url
+                print a_playerstate.cards
+                a_playerstate.cards.append(card.id)
+                a_playerstate.save()
+        game.save()
+    
+    def test_creation(self):
+        '@type game: Game'
+        game = Game.objects.get(board_id='board 1')
+        self.assertEqual(Card.objects.all().count(), 20)
+        self.assertEqual(Player.objects.all().count(), 3)
+        self.assertEqual(game.board_id, 'board 1')
+        self.assertEqual(game.playergamestates.count(), 3)
+        playergamestate = game.playergamestates.get(player_state_id='playerstate_player_url0')
+        #matches one single card
+        print '-------'
+        print playergamestate.cards
+        print '-------'
+        card_id = playergamestate.cards[0]
+        '@type card: Card'
+        card = Card.objects.get(url="url0")
+        self.assertEqual(card_id, card.id)
+        
+       
